0b136244d4091eab4b225fb2bdaace6fbfd4f5be
PKAnti <jarodrig@andrew.cmu.edu>
2017-08-02 18:29:44 -0400

    Fixed runBot.sh to run and restart properly

--- runBot.sh
+++ runBot.sh
@@ -1,18 +1,20 @@
 # !/bin/bash
+mv token.txt Webster-Bot/token.txt
+cd Webster-Bot
+git pull
 
 timestamp() {
-    date +"[%T]"
+        date +"[%T]"
 }
 
-((ret_code=1))
-> output.log
-while [[ $ret_code == 1 ]]; do
-    python magolor.py > output.log
-    ret_code=$?
-    if [[ $ret_code == 1 ]]; then
-        echo $(timestamp) "Bot is restarting." > output.log
-    else
-        echo $(timestamp) "Bot has shut down." > output.log
-    fi
+((ret_code=0))
+while [[ $ret_code == 0 ]]; do
+        python3 magolor.py >> output.log
+        ret_code=$?
+        if [[ ret_code == 0 ]]; then
+                echo $(timestamp) "Bot is restarting." >> output.log
+                git pull
+        else
+                echo $(timestamp) "Bot has shut down." >> output.log
+        fi
 done
-
